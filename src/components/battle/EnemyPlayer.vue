51ac45f7f3bc7289ff943b1c4bc7cefe5bc97eb7
鸭脖yaboo <863176846@qq.com>
2022-05-03 10:23:47 +0800

    fix: enemy card

--- src/components/battle/EnemyPlayer.vue
+++ src/components/battle/EnemyPlayer.vue
@@ -3,15 +3,18 @@
     <div flex>
       <img width="50" height="50" />
       <div flex flex-col items-start text-3 m-l-1>
-        <div>id:{{ id }}</div>
+        <div>No.{{ id }}</div>
         <div>{{ name }}</div>
       </div>
     </div>
     <div flex items-center justify="around" w="100%">
       <i class="i mdi:cards-outline"></i>
       <div>{{ cardNum }}</div>
-      <div w-5 h-5 b="rounded-50" :bg="lastCard.color">
-        <i :class="iconClass" lh-5 color="white"></i>
+      <div w-5 h-5 :style="{
+        backgroundColor: lastCard.color,
+        borderRadius: '50%',
+      }">
+        <i :class="iconClass" lh-5 c="white"></i>
       </div>
     </div>
   </div>
@@ -25,22 +28,20 @@ interface PlayerProps {
   lastCard?: CardProps
 }
 
-
-const props  = withDefaults(defineProps<PlayerProps>(), {
-  name: 'merlin',
-  id: '111',
-  cardNum: 3,
+const props = withDefaults(defineProps<PlayerProps>(), {
+  name: '无玩家',
+  id: '-1',
+  cardNum: 0,
   lastCard: () => {
     return {
-      type: 'number-1',
-      color: 'red',
-      icon: 'mdi:cards-outline'
+      type: 'ban',
+      color: 'gray',
+      icon: 'pixelarticons:downasaur'
     }
   }
 })
 
 const iconClass = computed(() => `i ${props.lastCard.icon}`)
-
 </script>
 
 <style scoped>
--- src/types/card.ts
+++ src/types/card.ts
@@ -1,5 +1,6 @@
 type zeroToNine = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
 type oneToNine = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+type cardColor = '#FF6666'| '#99CC66'| '#99CCFF'| '#FFCC33'
 
 type CardNumberType<T extends number> = `number-${T}`;
 type CardOtherType = 'add-2' | 'add-4' | 'exchange' | 'palette' | 'ban'
@@ -7,5 +8,5 @@ type CardOtherType = 'add-2' | 'add-4' | 'exchange' | 'palette' | 'ban'
 declare interface CardProps {
   icon: string,
   type: CardNumberType<zeroToNine> | CardOtherType
-  color: string
+  color:cardColor | string
 }
