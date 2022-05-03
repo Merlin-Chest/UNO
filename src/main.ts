07a5ca6451357b91cee72fd25d2a1869f8bf89e9
鸭脖yaboo <863176846@qq.com>
2022-05-03 11:56:47 +0800

    feat: webSocket config

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,29 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from 'virtual:generated-pages'
-import { createPinia } from 'pinia'
 import App from './App.vue'
 
+// css 样式
 import '@unocss/reset/tailwind.css'
 import './styles/main.css'
 import 'uno.css'
 import '~/styles/iconfont/iconfont.css'
 
+export const app = createApp(App)
 
-const app = createApp(App)
-app.use(createPinia())
+// websocket相关
+import { useSocketStoreWithOut } from '~/store/socket';
+import VueNativeSock from "vue-native-websocket-vue3";
+const socketStore = useSocketStoreWithOut();
+app.use(VueNativeSock, "", {
+  store: socketStore
+});
+
+// 路由管理
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
 app.use(router)
 app.mount('#app')
+
