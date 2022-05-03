b1e5109637990392f6b27945999f3a390513d40d
鸭脖yaboo <863176846@qq.com>
2022-05-03 11:12:45 +0800

    feat: avater

--- src/components/battle/EnemyArea.vue
+++ src/components/battle/EnemyArea.vue
@@ -1,42 +1,31 @@
 <template>
   <div flex flex-wrap>
-    <EnemyPlayer v-for="player in players" :key="player.id" 
-    :name="player.name" :id="player.id" :num="player.num" />
+    <EnemyPlayer v-for="player in players" :key="player.id" :name="player.name" :id="player.id" :cardNum="player.cardNum" :last-card="player.lastCard" />
   </div>
 </template>
 
 <script setup lang="ts">
+
 const players = ref([{
-  name: 'merlin',
+  name: 'merlinmerlinmerlinmerlin',
   id: '111',
-  num: 0
+  cardNum: 0,
+  lastCard:{
+    type: 'number-1',
+    color: 'red',
+    icon: 'pixelarticons:downasaur'
+  }
 },{
   name: 'merlin',
   id: '111',
-  num: 0
-},{
-  name: 'merlin',
-  id: '111',
-  num: 0
-},{
-  name: 'merlin',
-  id: '111',
-  num: 0
-},{
-  name: 'merlin',
-  id: '111',
-  num: 0
-},{
-  name: 'merlin',
-  id: '111',
-  num: 0
-},{
-  name: 'merlin',
-  id: '111',
-  num: 0
+  cardNum: 0,
+  lastCard:{
+    type: 'number-1',
+    color: 'green',
+    icon: 'pixelarticons:downasaur'
+  }
 }])
 </script>
 
 <style scoped>
-
 </style>
--- src/components/battle/EnemyPlayer.vue
+++ src/components/battle/EnemyPlayer.vue
@@ -1,27 +1,25 @@
 <template>
-  <div flex flex-col items-center justify="between" b="dashed 4 rounded-2 yellow-200" box="border" p-2 m-2>
+  <div flex flex-col items-center justify="between" b="dashed 4 rounded-2" :style="{
+    borderColor: lastCard.color
+  }" box="border" p-2 m-2>
     <div flex>
-      <img width="50" height="50" />
+      <img width="50" height="50" :src="avatarSrc" b="rounded-2" />
       <div flex flex-col items-start text-3 m-l-1>
-        <div>No.{{ id }}</div>
-        <div>{{ name }}</div>
+        <div max-w-12 max-w-20 overflow-hidden text-ellipsis>No.{{ id }}</div>
+        <div min-w-8 max-w-20 overflow-hidden text-ellipsis>{{ name }}</div>
       </div>
     </div>
-    <div flex items-center justify="around" w="100%">
-      <i class="i mdi:cards-outline"></i>
+    <div flex items-center w-4 h-4 lh-4 justify="around" w="100%" m-t-1  :style="{ color: lastCard.color }">
+      <i w-4 h-4 class="i mdi:cards-outline"></i>
       <div>{{ cardNum }}</div>
-      <div w-5 h-5 :style="{
-        backgroundColor: lastCard.color,
-        borderRadius: '50%',
-      }">
-        <i :class="iconClass" lh-5 c="white"></i>
-      </div>
+      <i w-4 h-4 :class="iconClass"></i>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-interface PlayerProps {
+import { generateFromString } from 'generate-avatar'
+export interface PlayerProps {
   id: string,
   name: string,
   cardNum: number,
@@ -34,7 +32,7 @@ const props = withDefaults(defineProps<PlayerProps>(), {
   cardNum: 0,
   lastCard: () => {
     return {
-      type: 'ban',
+      type: 'number-1',
       color: 'gray',
       icon: 'pixelarticons:downasaur'
     }
@@ -42,6 +40,7 @@ const props = withDefaults(defineProps<PlayerProps>(), {
 })
 
 const iconClass = computed(() => `i ${props.lastCard.icon}`)
+const avatarSrc = computed(() => `data:image/svg+xml;utf8,${generateFromString(props.id + props.name)}`)
 </script>
 
 <style scoped>
